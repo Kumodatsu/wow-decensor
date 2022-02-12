9c2a5278e6d8bd7643811342f80403ef86f4a295
Kumodatsu <dragonkumo@gmail.com>
2022-02-12 20:44:23 +0100

    Restore more emotes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.0][] - 2022-02-12
+### Added
+- The following emotes have been restored: `/belch`, `/burp`, `/cough`, `/fart`,
+`/shake`, `/rear`, `/smell`, `/stink`.
+
 ## [0.1.0][] - 2022-02-12
 ### Added
 - Initial version of the addon.
 - The following emote has been re-added: `/moan`.
 - The following emotes can now be used on a target again: `/spit`, `/moon`.
 
+[0.2.0]: <https://github.com/Kumodatsu/wow-decensor/releases/tag/v0.2.0>
 [0.1.0]: <https://github.com/Kumodatsu/wow-decensor/releases/tag/v0.1.0>
--- addons/Decensor/src/main.lua
+++ addons/Decensor/src/main.lua
@@ -1,13 +1,36 @@
 local DCS, addon_name = ...
 
-local register_emote = function(cmd, no_target_msg, target_msg)
-    local id = cmd:upper()
-    _G[("SLASH_%s1"):format(id)] = ("/%s"):format(cmd)
-    SlashCmdList[id] = function(arg)
-        local has_target = UnitExists "target"
-        SendChatMessage(has_target and target_msg or no_target_msg, "EMOTE")
+local register_emote = function(cmds, no_target_msg, target_msg)
+    if type(cmds) ~= "table" then
+        cmds = { cmds }
+    end
+    for _, cmd in ipairs(cmds) do
+        local id = cmd:upper()
+        _G[("SLASH_%s1"):format(id)] = ("/%s"):format(cmd)
+        SlashCmdList[id] = function(arg)
+            local has_target = UnitExists "target"
+            SendChatMessage(has_target and target_msg or no_target_msg, "EMOTE")
+        end
     end
 end
+
+register_emote(
+    { "belch", "burp" },
+    "lets out a loud belch.",
+    "burps rudely in %t's face."
+)
+
+register_emote(
+    "cough",
+    "lets out a hacking cough.",
+    "coughs at %t."
+)
+
+register_emote(
+    "fart",
+    "farts loudly. Whew...what stinks?",
+    "brushes up against %t and farts loudly."
+)
 
 register_emote(
     "moan",
@@ -16,13 +39,25 @@ register_emote(
 )
 
 register_emote(
-    "spit",
-    "spits on the ground.",
-    "spits on %t."
-)
-
-register_emote(
     "moon",
     "drops their trousers and moons everyone.",
     "drops their trousers and moons %t."
+)
+
+register_emote(
+    { "shake", "rear" },
+    "shakes their rear.",
+    "shakes their rear at %t."
+)
+
+register_emote(
+    { "smell", "stink" },
+    "smells the air around them. Wow, someone stinks!",
+    "smells %t. Wow, someone stinks!"
+)
+
+register_emote(
+    "spit",
+    "spits on the ground.",
+    "spits on %t."
 )
